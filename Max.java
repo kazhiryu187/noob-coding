0d419fe7a789f7a1ce717057a60ba1ab1f337bb3
KazHiryu187 <47704184+kazhiryu187@users.noreply.github.com>
2020-04-11 17:03:40 -0400

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,14 @@
+public class Max{
+	
+	public static void main(String[] args){
+		
+		System.out.println("----------------------------------");
+		System.out.println("Byte = " + Byte.MAX_VALUE);
+		System.out.println("Short = " + Short.MAX_VALUE);
+		System.out.println("Integer = " + Integer.MAX_VALUE);
+		System.out.println("Long = " + Long.MAX_VALUE);
+		System.out.println("Float = " + Float.MAX_VALUE);
+		System.out.println("Double = " + Double.MAX_VALUE);
+		System.out.println("----------------------------------");
+	}
+}
