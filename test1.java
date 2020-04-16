6e97bc5d6773f2bfc1c6ef0f5dfd8b6eb6d6dfb2
KazHiryu187 <47704184+kazhiryu187@users.noreply.github.com>
2020-04-16 13:12:46 -0400

    Testing out changing values on vars, other things

---
+++ test1.java
@@ -0,0 +1,17 @@
+public class test1{
+	
+	static byte B1 = 120;
+	
+	public static void main(String[] args){
+		
+		byte A1 = 5;
+		
+		System.out.println(A1);
+		
+		A1 = 2;
+		
+		System.out.println(A1);
+		System.out.println(" ");
+		System.out.println(B1);
+	}
+}
