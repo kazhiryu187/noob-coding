6a618a61ee4a5a42aacbc6bf89f7cd6fdbaecaf9
KazHiryu187 <47704184+kazhiryu187@users.noreply.github.com>
2020-04-30 20:14:00 -0400

    Add files via upload

---
+++ arrow.java
@@ -0,0 +1,19 @@
+public class arrow {
+	
+	static String St1 = "dire wire!";
+	static int Num1 = 4586;
+	static final double Db1 = 1.788786534;
+	
+	public static void main(String[] args) {
+		
+		double Db2 = Num1 + Db1;
+		
+		System.out.println(" ");
+		System.out.println(Db1);
+		System.out.println("-------------------");
+		System.out.println(Db2);
+		System.out.println("-------------------");
+		System.out.println(St1);
+		System.out.println(" ");
+	}
+}
---
+++ arrow2.java
@@ -0,0 +1,10 @@
+public class arrow2 extends arrow {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Db1);
+		System.out.println(St1);
+		System.out.println(Num1);
+		
+	}
+}
